2aa9804b06b7e786b6f62f05bcce52e9d5135032
MarceloLopezGuzman <173215@ids.upchiapas.edu.mx>
2020-07-04 00:56:58 -0500

    apis funcional

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 Route::post('/login', 'LoginController@login');
 Route::post('/registro-proveedor', 'ProveedorController@register');
 Route::post('/registro-gerente', 'GerenteController@register');
@@ -28,25 +29,23 @@ Route::group(['middleware' => 'jwt.auth'], function () { // Antes era:  auth.jwt
     Route::post('/logout', 'AuthController@logout');
 
     // API negocios
-    Route::middleware('jwt.auth:api')->put('/actualizar-negocio/{id}','NegociosController@update', function (Request $request) {
+    Route::middleware('jwt.auth:api')->put('/actualizar-negocio/{id}', 'NegociosController@update', function (Request $request) {
         return $request->negocios();
     });
-    Route::middleware('jwt.auth:api')->delete('borrar-negocio/{id}','NegociosController@destroy');
+    Route::middleware('jwt.auth:api')->delete('borrar-negocio/{id}', 'NegociosController@destroy');
 
 
     // API de productos
-    Route::middleware('jwt.auth:api')->put('/actualizar-producto/{id}','ProductoController@update', function (Request $request) {
+    Route::middleware('jwt.auth:api')->put('/actualizar-producto/{id}', 'ProductoController@update', function (Request $request) {
         return $request->producto();
     });
-    Route::middleware('jwt.auth:api')->delete('borrar-producto/{id}','ProductoController@destroy');
-
+    Route::middleware('jwt.auth:api')->delete('borrar-producto/{id}', 'ProductoController@destroy');
 
     //API marcas
-    Route::middleware('jwt.auth:api')->put('/actualizar-marca/{id}','MarcaController@update', function (Request $request) {
+    Route::middleware('jwt.auth:api')->put('/actualizar-marca/{id}', 'MarcaController@update', function (Request $request) {
         return $request->marca();
     });
-    Route::middleware('jwt.auth:api')->delete('borrar-marca/{id}','MarcaController@destroy');
-    
+    Route::middleware('jwt.auth:api')->delete('borrar-marca/{id}', 'MarcaController@destroy');
 });
 
 
