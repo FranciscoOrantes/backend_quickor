6b33bbe0c58816361440e7ce33d64fe59d5a0f1b
FranciscoOrantes <173222@ids.upchiapas.edu.mx>
2020-07-17 01:47:07 -0500

    prueba password

--- resources/views/password.blade.php
+++ resources/views/password.blade.php
@@ -8,7 +8,7 @@
 <body>
     <p>Ha solicitado un código para actualización de contraseña</p>
     <p>Si usted no ha sido porfavor contacte a soporte</p>
-    <p>{{$codigo}}</p>
+    <p></p>
     
     <p>QUICKOR, AL ALCANCE DE TU TIENDA</p>
 </body>
