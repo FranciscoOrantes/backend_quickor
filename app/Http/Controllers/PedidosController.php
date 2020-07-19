d5556af5ce97285a92b638245819ba9f79d33ef7
FranciscoOrantes <173222@ids.upchiapas.edu.mx>
2020-07-19 17:45:32 -0500

    correccion json pedidos

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -17,10 +17,9 @@ class PedidosController extends Controller
            $num_pedido_actual = $num_pedido_actual+1;
        }
 
-       $json = $request->input('json', null);
-       $params = json_decode($json);
-       $params_array = json_decode($json, true);
-        foreach ($params_array AS $param => $paramdata){
+       $params_array = $request->json; 
+      
+        foreach ($params_array as $param => $paramdata){
             $pedido = new Pedidos();
             $pedido->producto_id = $paramdata['producto_id'];
             $pedido->proveedor_id = $paramdata['proveedor_id'];
