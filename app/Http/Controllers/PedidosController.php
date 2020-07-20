032be734cc5c4ada891f6cbafb0a85041395b89e
FranciscoOrantes <173222@ids.upchiapas.edu.mx>
2020-07-19 20:49:08 -0500

    Prueba 11 pedidos

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -19,7 +19,7 @@ class PedidosController extends Controller
        $data = request()->all();
        $contador = 0;
        foreach ($data['datos']['data'] as $key => $value) {
-           $contador = $contador+1;
+          
            $pedido = new Pedidos();
             $pedido->producto_id = dd($value[$contador]['producto_id']);
             $pedido->proveedor_id = dd($value[$contador]['proveedor_id']);
@@ -31,6 +31,7 @@ class PedidosController extends Controller
             $pedido->cantidad = dd($value[$contador]['cantidad']);
             
             $pedido->save();
+            $contador = $contador+1;
        }
         
        
