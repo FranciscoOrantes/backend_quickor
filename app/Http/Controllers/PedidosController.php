77358f1a8e58f18a97628e0784de73f26d942322
FranciscoOrantes <173222@ids.upchiapas.edu.mx>
2020-07-19 22:18:47 -0500

    registrar pedidos v2

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -11,7 +11,12 @@ class PedidosController extends Controller
         $json = request()->all();
        $fechaActual = new DateTime();
        $fechaActual->format('d-m-Y');
-      
+      $num_pedido_actual =  Pedidos::max('num_pedido');
+       if($num_pedido_actual==null){
+           $num_pedido_actual=1;
+       }else{
+           $num_pedido_actual = $num_pedido_actual+1;
+       }
     
         
        for ($i=0; $i <sizeof($json) ; $i++) { 
@@ -21,8 +26,8 @@ class PedidosController extends Controller
         $pedido->gerente_id = $json[$i]['gerente_id'];
         $pedido->status = $json[$i]['status'];
         $pedido->status_pago = $json[$i]['status_pago'];
-        $pedido->fecha = $json[$i]['fecha'];
-        $pedido->num_pedido = $json[$i]['num_pedido'];
+        $pedido->fecha = $fechaActual;
+        $pedido->num_pedido = $num_pedido_actual;
         $pedido->cantidad = $json[$i]['cantidad'];
         
         $pedido->save();
