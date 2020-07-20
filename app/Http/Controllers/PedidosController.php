f5c35f353599fd8e371fa8837166e52e5b6cc3af
FranciscoOrantes <173222@ids.upchiapas.edu.mx>
2020-07-19 19:16:29 -0500

    prueba pedidos 4

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -17,19 +17,17 @@ class PedidosController extends Controller
            $num_pedido_actual = $num_pedido_actual+1;
        }
 
-       $json = $request->input('json', null);
-       $params = json_decode($json);
-       $params_array = (array)json_decode($json, true);
-        foreach ($params_array as $param){
+      
+        foreach ($request->json as $param => $paramdata){
             $pedido = new Pedidos();
-            $pedido->producto_id = $param['producto_id'];
-            $pedido->proveedor_id = $param['proveedor_id'];
-            $pedido->gerente_id = $param['gerente_id'];
-            $pedido->status = $param['status'];
-            $pedido->status_pago = $param['status_pago'];
-            $pedido->fecha = $param['fecha'];
-            $pedido->num_pedido = $param['num_pedido'];
-            $pedido->cantidad = $param['cantidad'];
+            $pedido->producto_id = $paramdata['producto_id'];
+            $pedido->proveedor_id = $paramdata['proveedor_id'];
+            $pedido->gerente_id = $paramdata['gerente_id'];
+            $pedido->status = $paramdata['status'];
+            $pedido->status_pago = $paramdata['status_pago'];
+            $pedido->fecha = $paramdata['fecha'];
+            $pedido->num_pedido = $paramdata['num_pedido'];
+            $pedido->cantidad = $paramdata['cantidad'];
             
             $pedido->save();
        }
