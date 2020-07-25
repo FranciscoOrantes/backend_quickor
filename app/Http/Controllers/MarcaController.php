bf8c4c0e3428d2670c07f475e1defde45d75dc25
FranciscoOrantes <173222@ids.upchiapas.edu.mx>
2020-07-25 02:05:37 -0500

    prueba logo 3

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Marca;
+use App\Imagenes_marcas;
+use Cloudder;
 class MarcaController extends Controller
 {
     public function register(Request $request)
@@ -12,8 +14,28 @@ class MarcaController extends Controller
         $marca = new Marca();
         $marca->nombre = $request->nombre;
         $marca->save();
+        $id = $marca->id;
+        $imagen = new Imagenes_marcas();
+    $this->validate($request,[
+        'image_name'=>'required|mimes:jpeg,bmp,jpg,png|between:1, 6000',
+    ]);
 
+    $image = $request->file('image_name');
 
+    $name = $request->file('image_name')->getClientOriginalName();
+
+    $image_name = $request->file('image_name')->getRealPath();;
+    Cloudder::upload($image_name, null);
+    list($width, $height) = getimagesize($image_name);
+
+       $image_url= Cloudder::show(Cloudder::getPublicId(), ["width" => $width, "height"=>$height]);
+
+       //save to uploads directory
+      
+    $imagen ->marca_id=$id;
+    $imagen->urlImagen=$image_url;
+    $imagen->nombre = $name;
+    $imagen->save();
         
         return $marca;
 
