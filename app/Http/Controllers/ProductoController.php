ef87606a52456c9cf4acac7ce933a09ca1050248
FranciscoOrantes <173222@ids.upchiapas.edu.mx>
2020-07-27 17:38:30 -0500

    hola xd5

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -120,11 +120,11 @@ class ProductoController extends Controller
 
     public function marcasXProveedorCercano(Request $request){
         $negocioId = $request->negocioId;
-        $proveedoresOrdenados=DB::select(DB::raw("SELECT CONCAT(proveedors.nombre,' ',proveedors.apellido_paterno,' ',proveedors.apellido_materno) AS nombreCompleto,proveedors.id,marcas.nombre,
+        $proveedoresOrdenados=DB::select("SELECT CONCAT(proveedors.nombre,' ',proveedors.apellido_paterno,' ',proveedors.apellido_materno) AS nombreCompleto,proveedors.id,marcas.nombre,
         (acos(sin(radians(CAST(negocios.latitud AS DECIMAL))) * sin(radians(CAST(proveedors.latitud AS DECIMAL))) +
         cos(radians(CAST(negocios.latitud AS DECIMAL))) * cos(radians(CAST(proveedors.latitud AS DECIMAL))) *
         cos(radians(CAST(negocios.longitud AS DECIMAL)) - radians(CAST(proveedors.longitud AS DECIMAL)))) * 6371) as distanciaKm FROM proveedors,negocios,marcas,productos
-        WHERE negocios.id='$negocioId' AND productos.marca_id=marcas.id GROUP BY proveedors.id,marcas.nombre,nombreCompleto,distanciaKm ORDER BY distanciaKm ASC"));
+        WHERE negocios.id=11 AND productos.marca_id=marcas.id GROUP BY proveedors.id,marcas.nombre,nombreCompleto,distanciaKm ORDER BY distanciaKm ASC");
 
 
 
